3e8d2c49205811ac2fcc783ae1f7843c823a9f5e
Danzam97 <65907515+Danzam97@users.noreply.github.com>
2020-06-09 04:52:02 -0600

    Add files via upload

---
+++ Guia.Designer.vb
@@ -0,0 +1,328 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Almacenes
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtAlmacen1 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen2 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen3 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen4 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen5 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen6 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen7 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen8 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen9 = New System.Windows.Forms.TextBox()
+        Me.txtAlmacen10 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnMenu = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtPromedio = New System.Windows.Forms.TextBox()
+        Me.txtMayores = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtAlmacen1
+        '
+        Me.txtAlmacen1.Location = New System.Drawing.Point(53, 98)
+        Me.txtAlmacen1.Name = "txtAlmacen1"
+        Me.txtAlmacen1.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen1.TabIndex = 0
+        '
+        'txtAlmacen2
+        '
+        Me.txtAlmacen2.Location = New System.Drawing.Point(177, 98)
+        Me.txtAlmacen2.Name = "txtAlmacen2"
+        Me.txtAlmacen2.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen2.TabIndex = 1
+        '
+        'txtAlmacen3
+        '
+        Me.txtAlmacen3.Location = New System.Drawing.Point(294, 98)
+        Me.txtAlmacen3.Name = "txtAlmacen3"
+        Me.txtAlmacen3.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen3.TabIndex = 2
+        '
+        'txtAlmacen4
+        '
+        Me.txtAlmacen4.Location = New System.Drawing.Point(414, 98)
+        Me.txtAlmacen4.Name = "txtAlmacen4"
+        Me.txtAlmacen4.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen4.TabIndex = 3
+        '
+        'txtAlmacen5
+        '
+        Me.txtAlmacen5.Location = New System.Drawing.Point(529, 98)
+        Me.txtAlmacen5.Name = "txtAlmacen5"
+        Me.txtAlmacen5.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen5.TabIndex = 4
+        '
+        'txtAlmacen6
+        '
+        Me.txtAlmacen6.Location = New System.Drawing.Point(53, 190)
+        Me.txtAlmacen6.Name = "txtAlmacen6"
+        Me.txtAlmacen6.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen6.TabIndex = 5
+        '
+        'txtAlmacen7
+        '
+        Me.txtAlmacen7.Location = New System.Drawing.Point(177, 190)
+        Me.txtAlmacen7.Name = "txtAlmacen7"
+        Me.txtAlmacen7.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen7.TabIndex = 6
+        '
+        'txtAlmacen8
+        '
+        Me.txtAlmacen8.Location = New System.Drawing.Point(294, 190)
+        Me.txtAlmacen8.Name = "txtAlmacen8"
+        Me.txtAlmacen8.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen8.TabIndex = 7
+        '
+        'txtAlmacen9
+        '
+        Me.txtAlmacen9.Location = New System.Drawing.Point(414, 190)
+        Me.txtAlmacen9.Name = "txtAlmacen9"
+        Me.txtAlmacen9.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen9.TabIndex = 8
+        '
+        'txtAlmacen10
+        '
+        Me.txtAlmacen10.Location = New System.Drawing.Point(529, 190)
+        Me.txtAlmacen10.Name = "txtAlmacen10"
+        Me.txtAlmacen10.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlmacen10.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 17)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Almacen 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(174, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 17)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Almacen 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(291, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 17)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Almacen 3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(411, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 17)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Almacen 4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(526, 68)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 17)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Almacen 5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(50, 161)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(74, 17)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Almacen 6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(174, 161)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 17)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Almacen 7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(291, 161)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(74, 17)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Almacen 8"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(411, 161)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(74, 17)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Almacen 9"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(526, 161)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(82, 17)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Almacen 10"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(369, 296)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(116, 39)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Ingresar ventas"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnMenu
+        '
+        Me.btnMenu.Location = New System.Drawing.Point(513, 296)
+        Me.btnMenu.Name = "btnMenu"
+        Me.btnMenu.Size = New System.Drawing.Size(116, 39)
+        Me.btnMenu.TabIndex = 21
+        Me.btnMenu.Text = "Menu"
+        Me.btnMenu.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(174, 234)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(68, 17)
+        Me.Label11.TabIndex = 23
+        Me.Label11.Text = "Promedio"
+        '
+        'txtPromedio
+        '
+        Me.txtPromedio.Location = New System.Drawing.Point(177, 254)
+        Me.txtPromedio.Name = "txtPromedio"
+        Me.txtPromedio.Size = New System.Drawing.Size(100, 22)
+        Me.txtPromedio.TabIndex = 24
+        '
+        'txtMayores
+        '
+        Me.txtMayores.Location = New System.Drawing.Point(53, 254)
+        Me.txtMayores.Multiline = True
+        Me.txtMayores.Name = "txtMayores"
+        Me.txtMayores.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtMayores.Size = New System.Drawing.Size(100, 101)
+        Me.txtMayores.TabIndex = 25
+        '
+        'Label12
+        '
+        Me.Label12.Location = New System.Drawing.Point(50, 217)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(103, 34)
+        Me.Label12.TabIndex = 26
+        Me.Label12.Text = "Mayores al promedio"
+        '
+        'Almacenes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(686, 385)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.txtMayores)
+        Me.Controls.Add(Me.txtPromedio)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.btnMenu)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtAlmacen10)
+        Me.Controls.Add(Me.txtAlmacen9)
+        Me.Controls.Add(Me.txtAlmacen8)
+        Me.Controls.Add(Me.txtAlmacen7)
+        Me.Controls.Add(Me.txtAlmacen6)
+        Me.Controls.Add(Me.txtAlmacen5)
+        Me.Controls.Add(Me.txtAlmacen4)
+        Me.Controls.Add(Me.txtAlmacen3)
+        Me.Controls.Add(Me.txtAlmacen2)
+        Me.Controls.Add(Me.txtAlmacen1)
+        Me.Name = "Almacenes"
+        Me.Text = "Almacenes"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtAlmacen1 As TextBox
+    Friend WithEvents txtAlmacen2 As TextBox
+    Friend WithEvents txtAlmacen3 As TextBox
+    Friend WithEvents txtAlmacen4 As TextBox
+    Friend WithEvents txtAlmacen5 As TextBox
+    Friend WithEvents txtAlmacen6 As TextBox
+    Friend WithEvents txtAlmacen7 As TextBox
+    Friend WithEvents txtAlmacen8 As TextBox
+    Friend WithEvents txtAlmacen9 As TextBox
+    Friend WithEvents txtAlmacen10 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnMenu As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txtPromedio As TextBox
+    Friend WithEvents txtMayores As TextBox
+    Friend WithEvents Label12 As Label
+End Class
---
+++ Guia.vb
@@ -0,0 +1,101 @@
+﻿Public Class Almacenes
+    Private Sub btnMenu_Click(sender As Object, e As EventArgs) Handles btnMenu.Click
+        MainMenu.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim almacen, suma As Integer
+        Dim promedio As Double
+        suma = 0
+        For i = 1 To 10 Step 1
+            almacen = InputBox("Ingrese las ventas mensuales de este almacen", i)
+            suma = suma + almacen
+            If i = 1 Then
+                txtAlmacen1.Text = almacen
+            ElseIf i = 2 Then
+                txtAlmacen2.Text = almacen
+            ElseIf i = 3 Then
+                txtAlmacen3.Text = almacen
+            ElseIf i = 4 Then
+                txtAlmacen4.Text = almacen
+            ElseIf i = 5 Then
+                txtAlmacen5.Text = almacen
+            ElseIf i = 6 Then
+                txtAlmacen6.Text = almacen
+            ElseIf i = 7 Then
+                txtAlmacen7.Text = almacen
+            ElseIf i = 8 Then
+                txtAlmacen8.Text = almacen
+            ElseIf i = 9 Then
+                txtAlmacen9.Text = almacen
+            ElseIf i = 10 Then
+                txtAlmacen10.Text = almacen
+            End If
+        Next
+        promedio = suma / 10
+        txtPromedio.Text = promedio
+
+        If Val(txtAlmacen1.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 1
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+
+        If Val(txtAlmacen2.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 2
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+        If Val(txtAlmacen3.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 3
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+        If Val(txtAlmacen4.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 4
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+        If Val(txtAlmacen5.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 5
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+        If Val(txtAlmacen6.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 6
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+        If Val(txtAlmacen7.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 7
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+        If Val(txtAlmacen8.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 8
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+        If Val(txtAlmacen9.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 9
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+        If Val(txtAlmacen10.Text) > promedio Then
+            txtMayores.Text &= "Almacen "
+            txtMayores.Text &= 10
+            txtMayores.Text &= Environment.NewLine
+            txtMayores.SelectionStart = txtMayores.Text.Length
+        End If
+    End Sub
+End Class
